41bb2584503528aa494de7b7f049b67b02e33238
Bruno Schriber <bruno.schriber@xpera.ch>
2021-09-01 22:36:48 +0200

    inital commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+ <v-app>
+  <v-app-bar app>
+    <!-- -->
+    <v-app-bar-title>Basic Example</v-app-bar-title>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
